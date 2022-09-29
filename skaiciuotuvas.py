21fa7bce1bed8e176dea330e0928b42800c77ab6
Arnoldas Kundrotas <arnokund@gmail.com>
2022-09-29 20:37:32 +0300

    naujas_pilnas_skaic

--- skaiciuotuvas.py
+++ skaiciuotuvas.py
@@ -1,16 +1,50 @@
-def skaiciuoti():
-    veiksmas = input("")
+#naujas dublis ir pritaikytas gui
+#sudetis, atimtis, daugyba, dalyba
 
-    sk1 = float(input("Iveskite pirma skaiciu: "))
-    sk2 = float(input("Iveskite antra skaiciu: "))
+def sudetis(x, y):
+    return x + y
 
-    #sudetis
-    if veiksmas == "+":
-        print("{} + {} = ".format(sk1, sk2))
-        print(sk1 + sk2)
-    #atimtis
-    if veiksmas == "-":
-        print("{} - {} = ".format(sk1, sk2))
-    #daugyba
-    if veiksmas == "*":
-        print
+def atimtis(x, y):
+    return x - y
+
+def daugyba(x, y):
+    return x * y
+
+def dalyba(x, y):
+    return x / y
+
+print("Pasirinkite operacija")
+print("1.Sudetis")
+print("2.Atimtis")
+print("3.Daugyba")
+print("4.Dalyba")
+
+while True:
+    pasirinkimas = input("Iveskite pasirinkima(1/2/3/4): ")
+
+    if pasirinkimas in ("1", "2", "3", "4"):
+        sk1 = float(input("Iveskite pirma skaiciu: "))
+        sk2 = float(input("Iveskite antra skaiciu: "))
+
+        if pasirinkimas == "1":
+            print(sk1, "+", sk2, "=", sudetis(sk1, sk2))
+
+        elif pasirinkimas == "2":
+            print(sk1, "-", sk2, "=", atimtis(sk1, sk2))
+
+        elif pasirinkimas == "3":
+            print(sk1, "*", sk2, "=", daugyba(sk1, sk2))
+
+        elif pasirinkimas == "4":
+            print(sk1, "/", sk2, "=", dalyba(sk1, sk2))
+
+        kitas_skaiciavimas = input("Ar darom dar viena skaiciavima? (T/N): ")
+        if kitas_skaiciavimas == "N":
+            break
+
+    else:
+        print("Netinkamas pasirinkimas")
+
+
+
+
